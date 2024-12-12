b4394807748045ca0ad91a7bf57e39c6cb9fc273
StormJH <user@Users-MacBook-Pro.local>
2024-12-12 19:07:55 +0000

    Last commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 **Instructions for Penguinassessed Project:**
 
-This project aims to analyse data (culmen width vs culmen length, across 3 species) from the “palmerpenguins” R package (Horst AM,et al, 2020). The repository includes files, containing the following information:
+This project aims to analyse data (culmen width vs culmen length, across 3 Penguin species) from the “palmerpenguins” R package (Horst AM,et al, 2020). The repository includes files, containing the following information:
 - "penguin_analysis.Rmd" contains an R markdown with various statistical analysis techniques analysing these data- a linear regressions, ANCOVA, and Z-test for differences between r coefficients (penguin_analysis.html, is a rendered version of the Rmd file). 
 - "functions", any functions used in this code are saved in seperate R files. 
 - "renv.lock" file contains all the package dependencies required for this code, use "renv::restore()" to instate all the package-versions required for this code. 
--- functions/Z-test_for_correlation_coefficients.R
+++ functions/Z-test_for_correlation_coefficients.R
@@ -1,46 +1,42 @@
+# Defining a function (saved in functions folder, "Z-test_for_correlation_coefficients.R") to analyse statistical significance of the difference between correlation coefficients across species, data= data source (penguins_clean), var1= variable 1 (culmen length), var2= variable 2 (culmen depth), and species= species column (species).
 
-# This function analyses the statistical significance of the difference between correlation coefficients across species, data= data source (penguins_clean), var1= variable 1 (culmen length), var2= variable 2 (culmen depth), and species_col= species column name (species).
-
-compare_species_correlations <- function(data, var1, var2, species_col) {
+compare_species_correlations <- function(data, var1, var2, species) {
   
   # Creating a new variable (correlation_results)  that contains a species' Z score, and it's respective SE.
-  correlation_results <- data %>%
-    group_by(!!sym(species_col)) %>%
-    summarise(
-      correlation = cor(!!sym(var1), !!sym(var2), use = "complete.obs"),
-      n = n()  
-    ) %>%
-    # Calculates a correlation coefficient between variable 1 and 2 for each species, calculating sample size for each species
+  Z_stat_results <- data %>%
+    group_by({{species}}) %>%
+    summarise(correlation = cor({{var1}}, {{var2}}, use = "complete.obs"),
+              n = n()) %>%
+    # Calculates a correlation coefficient between variable 1 and 2 for each species, only using complete rows and columns, calculating sample size for each species
     mutate(
       z = 0.5 * log((1 + correlation) / (1 - correlation)),  
-      # New column, transforming correlation coefficients into a Z score
+      # Creates a new column, transforming correlation coefficients into a Z score
       se = 1 / sqrt(n - 3)  
-      # New column containing the standard error for each Z score
+      # Creates a new column containing the standard error for each Z score
     )
   
+  species_list <- Z_stat_results %>% pull({{species}}) %>% unique()
+  # Identifies all the unique species names in a data frame, and stores them in the species_list vector ("Adelie", "Chinstrap", "Gentoo")
   
-  results_comparison <- combn(correlation_results[[species_col]], 2, simplify = FALSE) %>%
-    # Generates a list of all possible pairs of species in correlation_results.
+  Z_stat_results_comparison <- combn(species_list, 2, simplify = FALSE) %>%
+    # Generates a list of all possible pairs of species from the species_list vector, ["Adelie", "Chinstrap"], etc...
     lapply(function(pair) {
-      species_1 <- correlation_results %>% filter(!!sym(species_col) == pair[1])
-      species_2 <- correlation_results %>% filter(!!sym(species_col) == pair[2])
-      # Extract species 1 and 2 from each pair.
-      z_statistic <- abs(species_1$z - species_2$z) / sqrt(species_1$se^2 + species_2$se^2)
-      p_value <- 2 * (1 - pnorm(z_statistic))  
-      
-      # Calculates z-statistics between species pairs, calculating a two-tailed p-value corresponding with this statistic for every pair.
-      
+      species_1 <- Z_stat_results %>% filter({{species}} == pair[1])
+      species_2 <- Z_stat_results %>% filter({{species}} == pair[2])
+      # Extracts species 1 and 2 from each pair (e.g, pair[1]=["Adelie"] pair[2]=["Chinstrap"]) assigning them to separate vectors (species_1 and species_2).
+      z_stat <- abs(species_1$z - species_2$z) / sqrt(species_1$se^2 + species_2$se^2)
+      p_value <- 2 * (1 - pnorm(z_stat))  
+      # Calculates the absolute difference in z-statistic between every species pair, calculating a two-tailed p-value corresponding with this difference.
       data.frame(
         `Species 1` = pair[1],
         `Species 2` = pair[2],
         `Difference in Correlation Coefficient` = round(abs(species_1$correlation - species_2$correlation), 3),
-        `Z-Statistic` = round(z_statistic, 3),
-        `p-value` = round(p_value, 3)
-      )
+        `Z-Statistic` = round(z_stat, 3),
+        `p-value` = round(p_value, 3))
+      # Generates a data frame for the outputted data, altered column titles and rounded numbers to enhance readability.
     }) %>%
     bind_rows() 
-     #Combines rows for multiple species pairs
-  
-  return(results_comparison)
+  # Combines rows relating to different species pairs into a single data frame.
+  return(Z_stat_results_comparison)
+  # Send result of the function to the user, enabling its use outside of the function.
 }
-
