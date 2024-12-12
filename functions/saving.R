3e0dd3744e722d4ca74f65619040fe5e607024cb
StormJH <user@Users-MacBook-Pro.local>
2024-12-12 23:39:39 +0000

    Final commit

--- functions/saving.R
+++ functions/saving.R
@@ -1,13 +1,17 @@
-
-#This plot saves figures as a PNG.
+#Code to save a plot as a png file:
 
 save_plot_png <- function(plot_object, 
-                                  filename, size, res, scaling){
-  agg_png(filename, width   =  size, 
-          height  =  size, 
-          units   =  "cm", 
-          res     =  res, 
-          scaling =  scaling)
-  print(boxplot)
+                          filename, 
+                          size, 
+                          res, 
+                          scaling) {
+  # Create a png file with specified parameters.
+  
+  agg_png(filename, width = size, 
+          height = size, 
+          units = "cm", 
+          res = res, 
+          scaling = scaling)
+  print(plot_object)
   dev.off()
 }
